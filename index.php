8f189f84571c07d4ce3f703d4e22d55f29fa4b02
Simon LEONARD <git-1001af4@sinux.sh>
2021-02-01 23:22:35 +0100

    first index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
+<?php
+
+session_start();
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require('db.php');
+require('class/DocumentManager.php');
+
+$DOCS = new DocumentManager($DB);
 
