14db09df595cc2ad5f29d6e15c5489aca7d34416
Simon LEONARD <git-1001af4@sinux.sh>
2021-02-01 17:38:51 +0100

    connexion BDD + gitignore secret.php

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # OkyDoky
+
+## Base de donnée
+
+Pour connecter la base de donnée, il faut créer un fichier `secret.php` comme ceci : 
+
+```php
+<?php
+class Secret {
+    public const DB_HOST = 'localhost';
+    public const DB_USER = '<USER>';
+    public const DB_PASSWORD = '<PASSWORD>';
+    public const DB_NAME = '<BDD>';
+}
+```
--- db.php
+++ db.php
@@ -1,1 +1,17 @@
+<?php
 
+include "secret.php";
+
+$DB = mysqli(Secret::DB_HOST, Secret::DB_USER, Secret::DB_PASSWORD, Secret::DB_NAME);
+
+$DB->query("
+CREATE TABLE IF NOT EXISTS `document` (
+  `id_document` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` enum('link','image','pdf') NOT NULL,
+  `path` varchar(200) NOT NULL,
+  `visible` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"
+);
+
+unset(Secret);
