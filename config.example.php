dbe4005885a6773c212980d9de38b5922078a2e6
Simon LEONARD <git-1001af4@sinux.sh>
2021-02-01 18:23:17 +0100

    add example config file

---
+++ config.example.php
@@ -0,0 +1,18 @@
+<?php
+
+class Config {
+    // DB
+    public const DB_HOST = 'localhost';
+    public const DB_USER = '<USER>';
+    public const DB_PASSWORD = '<PASSWORD>';
+    public const DB_NAME = '<BDD>';
+
+    // Tables
+    public const TABLE_DOCUMENT = 'document';
+
+    // Files
+    public const DIR_DOCUMENT = __DIR__ . '/data/document';
+
+    //URLs
+    public const URL_ROOT = 'https://example.com/okydoky/';
+}
