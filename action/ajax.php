9b7e44914677431ef8b23d631d483bf08435b7f6
Simon LEONARD <git-1001af4@sinux.sh>
2021-02-21 17:55:14 +0100

    fix:  is array, not string; 'header location' moved per-function, not global

--- action/ajax.php
+++ action/ajax.php
@@ -1,5 +1,5 @@
 <?php
 
-function set_community(?string $match) {
+function set_community(?array $match) {
     User::current_user()->current_community = $_POST['id'];
 }
--- action/documents.php
+++ action/documents.php
@@ -1,10 +1,12 @@
 <?php
 
-function upload_document(?string $match) {
+function upload_document(?array $match) {
     $GLOBALS['docs']->add_document($_FILES["file"]);
+    header('Location: /');
 }
 
-function delete_document(?string $match) {
+function delete_document(?array $match) {
     $GLOBALS['docs']->del_document($_POST["id"]);
+    header('Location: /');
 }
 
