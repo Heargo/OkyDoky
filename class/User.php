f5a9fb16bcf192ccec9fb9bd495d54ac1c0424d7
Simon LEONARD <git-1001af4@sinux.sh>
2021-02-28 11:33:38 +0100

    body of methods written
    We can create users, get there attributes, set their password/email,
    check if a password is correct with the hash in database,
    know if an email/nickname is already used, find a user with its id,
    connect/disconnect a users.

--- class/User.php
+++ class/User.php
@@ -32,34 +32,56 @@ class User {
     // GETTERS
 
     /**
-     * Instantiate user from $id
+     * Instantiate user from an id
      */
-    public function __construct(mysqli $db, int $id);
+    public function __construct(mysqli $db, int $id) {
+        $this->_db = $db;
+        $this->_id = $id;
+                
+        if (!is_id_correct($this->_db, Config::TABLE_USER, $id)) {
+            throw new InvalidID('id "' . $id . '" in table "' . Config::TABLE_USER . '" isn`t correct.');
+        }
+
+        $sql = "SELECT * FROM `%s` WHERE `id_%s` = %s";
+        $sql = sprintf($sql, Config::TABLE_USER, Config::TABLE_USER, $id);
+        $row = $db->query($sql)->fetch_assoc();
+    
+        $this->_nickname = $row['nickname'];
+        $this->_display_name = $row['display_name'];
+        $this->_description = $row['description'];
+        $this->_profile_picture_url = $row['profile_picture'];
+        $this->_email = $row['email'];
+    }
+
+    /**
+     * Get id
+     */
+    public function id() : int { return $this->_id; }
 
     /**
      * Get nickname
      */
-    public function nickname() : string;
+    public function nickname() : string { return $this->_nickname; }
 
     /**
      * Get display name
      */
-    public function display_name() : string;
+    public function display_name() : ?string { return $this->_display_name; }
 
     /**
      * Get description
      */
-    public function description() : string;
+    public function description() : ?string { return $this->_description; }
 
     /**
      * Get email
      */
-    public function email() : string;
+    public function email() : ?string { return $this->_email; }
 
     /**
      * Get url to profile pic
      */
-    public function profile_pic() : string;
+    public function profile_pic() : string { return $this->_profile_picture_url; }
     
     // COMMUNITY INTERACTIONS
 
@@ -72,19 +94,19 @@ class User {
      * eg: $myuser->get_communities(P::OWNER); // will return communities where user is the owner
      *     $myuser->get_communities(P::VIEW); // will return communities where user is not banned from
      */
-    public function get_communities(?int $flags=null) : array;
+    //public function get_communities(?int $flags=null) : array;
 
     /**
      * Make the user join a community
      */
-    public function join(Community $comm) : bool;
+    //public function join(Community $comm) : bool;
 
     // AUTHENTIFICATION & PROFILE
 
     /**
      * Send an email to verify its authenticity
      */
-    public function verify_email() : bool;
+    //private function _verify_email() : bool;
 
     /**
      * Change user email.
@@ -92,40 +114,95 @@ class User {
      * TODO : This email must be confirmed.
      * @return bool True if successful
      */
-    public function set_email_to(string $new_email) : bool;
+    public function set_email_to(string $new_email) : bool {
+        $sanitized_email = filter_var($new_email, FILTER_SANITIZE_EMAIL);
+        if (filter_var($sanitized_email, FILTER_VALIDATE_EMAIL)) {
+            $sql = "UPDATE `%s` SET `email` = '%s', `email_verified` = 0 WHERE `id_%s` = %s";
+            $sql = sprintf($sql, Config::TABLE_USER, $sanitized_email, Config::TABLE_USER, $this->id());
+            $update_ok = $this->_db->query($sql);
+
+            if ($update_ok) {
+                $this->_email = $sanitized_email; //only if confirmed. So we need to stock old and new. Fix it
+            }
+
+            //$this->_verify_email();
+
+            return $update_ok;
+        }
+        return false;
+    }
 
     /**
-     * Check whenever the user email is confirmed
+     * Check whenever the user email is verified
      */
-    public function is_email_confirmed() : bool;
+    public function is_email_verified() : bool {
+        $sql = "SELECT `email_verified` FROM `%s` WHERE `id_%s` = %s";
+        $sql = sprintf($sql, Config::TABLE_USER, Config::TABLE_USER, $this->id());
+
+        $result = $this->_db->query($sql);
+
+        if ($result) {
+            $is_verified = $result->fetch_assoc()['email_verified'];
+            $is_verified = $is_verified == 1;
+            return $is_verified;
+        }
+
+        return false;
+    }
 
     /**
-     * Check if used password hash match $pwd_hash
+     * Check if $pwd is indeed the user's password
      *
      * @return bool True if valid.
      */
-    public function is_pwd_valid(string $pwd_hash) : bool;
+    public function is_pwd_correct(string $pwd) : bool {
+        $sql = "SELECT `pwd_hash` FROM `%s` WHERE `id_%s` = %s";
+        $sql = sprintf($sql, Config::TABLE_USER, Config::TABLE_USER, $this->id());
+        $result = $this->_db->query($sql);
+
+        if (!$result) { return false; } // No hash of password in DB.
+
+        $hash = $result->fetch_assoc()['pwd_hash'];
+
+        return password_verify($pwd, $hash);
+    }
 
     /**
      * Change password of user
+     *
+     * @return bool True if successful.
      */
-    public function set_pwd(string $pwd) : bool;
+    public function set_pwd(string $pwd) : bool {
+        $pwd_hash = password_hash($pwd, PASSWORD_DEFAULT);
+
+        $sql = "UPDATE `%s` SET `pwd_hash` = '%s' WHERE `id_%s` = %s";
+        $sql = sprintf($sql, Config::TABLE_USER, $pwd_hash, Config::TABLE_USER, $this->id());
+
+        return $this->_db->query($sql);
+    }
 
     /**
      * Send a link to user to reset password
      */
-    public function retrieve_pwd() : bool;
+    //public function retrieve_pwd() : bool;
 
     /**
      * Change profile picture
      *
      * @param $pic array is a picture from $_FILES.
-     * @return bool True if valid.
+     * @return bool True if successfull.
      */
-    public function set_profile_picture(array $pic) : bool;
+    //public function set_profile_picture(array $pic) : bool;
 
     /**
-     * Connect user if password hash is correct
+     * Change the display name
+     *
+     * @return bool True if successful.
+     */
+    //public function set_display_name(string $nickname) : bool;
+
+    /**
+     * Connect user if password is correct
      *
      * If passeword is correct, the user is stored in $_SESSION['user'].
      * To check if the user is connected, use User::is_connected().
@@ -133,14 +210,20 @@ class User {
      *
      * @return bool True if successful
      */
-    public function connect(string $pwd_hash) : bool;
+    public function connect(string $pwd) : bool {
+        $is_correct = $this->is_pwd_correct($pwd);
+        $_SESSION['user'] = $is_correct ? $this : $_SESSION['user'];
+        return $is_correct;
+    }
 
     /**
      * Disconnect user
      *
      * $_SESSION['user'] will be null.
      */
-    public function disconnect();
+    public function disconnect() : void {
+        unset($_SESSION['user']);
+    }
 
     // POSTS INTERACTIONS
 
@@ -149,17 +232,17 @@ class User {
      * 
      * @param $comm Community|null if you want posts from a specific community
      */
-    public function get_posts(?Community $comm) : array;
+    //public function get_posts(?Community $comm) : array;
 
     /**
      * User giving a good evaluation for a post
      */
-    public function up_vote(Post $post);
+    //public function up_vote(Post $post);
 
     /**
      * User giving a bad evaluation for a post
      */
-    public function down_vote(Post $post);
+    //public function down_vote(Post $post);
 
     /**
      * Check if a user has a certain permission
@@ -167,22 +250,22 @@ class User {
      * @param $flags a constant from class P or a group of constant joined by a "|" (pipe)
      * @return bool True if user has ALL permissions specified in $flags
      */
-    public function can(int $flags, Community $comm) : bool;
+    //public function can(int $flags, Community $comm) : bool;
 
     /**
      * Add a permission (or several) for a user on a community
      */
-    public function add_perm(int $flags, Community $comm);
+    //public function add_perm(int $flags, Community $comm);
 
     /**
      * Delete a permission (or several) for a user on a community
      */
-    public function del_perm(int $flags, Community $comm);
+    //public function del_perm(int $flags, Community $comm);
 
     /**
      * Set a permission (or several) for a user on a community
      */
-    public function set_perm(int $flags, Community $comm);
+    //public function set_perm(int $flags, Community $comm);
 
 
     //// STATIC
@@ -190,12 +273,17 @@ class User {
     /**
      * Return true whenever a user is connected
      */
-    public static function is_connected() : bool;
+    public static function is_connected() : bool {
+        return isset($_SESSION['user']) ? get_class($_SESSION['user']) == self::class : false;
+    }
 
     /**
      * Get current logged in user
      *
      * @return User|null Depending if a user is connected for this session or not
      */
-    public static function current() : ?User;
+    public static function current() : ?User {
+        // Do not return session var directly, it may not be User|null if overwritten
+        return self::is_connected() ? $_SESSION['user'] : null; 
+    }
 }
--- class/UserManager.php
+++ class/UserManager.php
@@ -7,25 +7,61 @@ class UserManager {
     private $_db;
 
     /**
+     * Instantiation a user manager
+     */
+    public function __construct(mysqli $db) {
+        $this->_db = $db;
+    }
+    /**
      * Get a user based on his ID.
      *
      * @return User|null Depends if a user exists with the $id.
      */
-    public function get_by_id(int $id) : ?User;
+    public function get_by_id(int $id) : ?User {
+        try {
+            return new User($this->_db, $id);
+        } catch (InvalidID $e) {
+            return null;
+        }
+    }
 
     /**
      * Get a user based on his nickname.
      *
      * @return User|null Depends if a user exists with the $nickname.
      */
-    public function get_by_nickname(string $nickname) : ?User;
+    //public function get_by_nickname(string $nickname) : ?User;
 
     /**
      * Get a user based on his email.
      *
      * @return User|null Depends if a user exists with the $email.
      */
-    public function get_by_email(string $email) : ?User;
+    //public function get_by_email(string $email) : ?User;
+
+    /**
+     * Know if nickname if free or already used
+     *
+     * @return bool True if the nickname can be used.
+     */
+    public function is_nickname_free(string $nickname) : bool {
+        $sql = "SELECT COUNT(`nickname`) FROM `%s` WHERE `nickname` = '%s'";
+        $sql = sprintf($sql, Config::TABLE_USER, $nickname);
+        $result = $this->_db->query($sql);
+        return $result ? $result->fetch_row()[0] == 0 : true;
+    }
+
+    /**
+     * Know if email if free or already used
+     *
+     * @return bool True if the email can be used.
+     */
+    public function is_email_free(string $email) : bool {
+        $sql = "SELECT COUNT(`email`) FROM `%s` WHERE `email` = '%s'";
+        $sql = sprintf($sql, Config::TABLE_USER, $email);
+        $result = $this->_db->query($sql);
+        return $result ? $result->fetch_row()[0] == 0 : true;
+    }
 
     /**
      * Register a user
@@ -38,12 +74,30 @@ class UserManager {
      *
      * @return bool True if successful.
      */
-    public function add_user(string $email, string $nickname, string $pwd) : bool;
+    public function add_user(string $email, string $nickname, string $pwd) : bool {
+        if (!$this->is_email_free($email)) { throw new NotUniqueEmail("Email $email is already used!"); }
+        if (!$this->is_nickname_free($nickname)) { throw new NotUniqueNickname("Nickname $nickname is already used!"); }
+
+        $sql = "INSERT INTO `%s` (`nickname`) VALUES ('%s')";
+        $sql = sprintf($sql, Config::TABLE_USER, $nickname);
+        $creation_ok = $this->_db->query($sql);
+
+        $email_ok = false;
+        $pwd_ok = false;
+        if ($creation_ok) {
+            // the user we are creating
+            $user = new User($this->_db, $this->_db->insert_id);
+            $email_ok = $user->set_email_to($email);
+            $pwd_ok = $user->set_pwd($pwd);
+        }
+        
+        return $creation_ok & $email_ok & $pwd_ok;
+    }
 
     /**
      * Delete a user
      *
      * @return bool True if successful.
      */
-    public function del_user(User $user) : bool;
+    //public function del_user(User $user) : bool;
 }
