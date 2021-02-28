9cc57baf7e01c5649ee2906c0e72ef235c646701
Simon LEONARD <git-1001af4@sinux.sh>
2021-03-01 00:19:03 +0100

    implementing authentification and registration

--- action.php
+++ action.php
@@ -8,4 +8,6 @@ foreach (glob("action/*.php") as $filename) {
 $ROUTES->bound_post("/document/new", 'upload_document')
        ->bound_post("/document/del", 'delete_document')
        ->bound_post("/ajax/community/current", 'set_community', ['id'])
+       ->bound_post('/signin', 'signin', ['login', 'password'])
+       ->bound_post('/signup', 'signup', ['nickname', 'email', 'password'])
 ;
