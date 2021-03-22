9c5f2afa2f84765b3d55569cba05406988d77a27
Hearstgo <hrey222@gmail.com>
2021-03-22 13:09:09 +0100

    fix style bubbles + new default img

--- page/topnav.php
+++ page/topnav.php
@@ -1,6 +1,6 @@
 <section class="topNav">
 	<h1 class="noselect">OkyDoky</h1>
-	<p><?= User::is_connected() ? User::current()->nickname() : "anonyme" ?></p>	
+	<!-- <p><?= User::is_connected() ? User::current()->nickname() : "anonyme" ?></p> -->	
 	<a href="#">
 		<img class="small-bubble-2 noselect" src="./img/img1.jpg" alt="communauté">
 	</a>
