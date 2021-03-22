f248aa30ece8fecd828a453ef288e64da91c7124
Hearstgo <hrey222@gmail.com>
2021-03-22 15:03:12 +0100

    fix

--- index.php
+++ index.php
@@ -27,7 +27,6 @@ $GLOBALS['communities'] = new CommunityManager($DB);
 $GLOBALS['posts'] = new PostManager($DB);
 $GLOBALS['db'] = $DB;
 if(!isset($_SESSION["current_community"])){
-    unset($_SESSION["current_community"]);
     $_SESSION["current_community"]=0;
 }
 /// Routes
