551fe31271461f6457a7120abaf037d1ef6cbb3e
Kroklethon <61271767+Kroklethon@users.noreply.github.com>
2021-04-01 14:07:42 +0200

    add join_community ajax

--- action/ajax.php
+++ action/ajax.php
@@ -4,3 +4,8 @@ function set_community(?array $match) {
     $_SESSION["current_community"] = (int) $_POST['id'];
     
 }
+function join_community(?array $match) {}
+    $id = (int) $_POST['id'];
+    $tempcomm = new Community($DB, $id);
+    $tempcomm->recruit(User::current());
+}
