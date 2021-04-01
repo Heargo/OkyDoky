df32f704d7e6090e204be76f2a517465974b8f50
Simon LEONARD <git-1001af4@sinux.sh>
2021-04-01 15:28:06 +0200

    Draft for ajax returning posts

--- action/ajax.php
+++ action/ajax.php
@@ -9,3 +9,15 @@ function join_community(?array $match) {
     $tempcomm = new Community($DB, $id);
     $tempcomm->recruit(User::current());
 }
+
+// function more_posts(?array $match) {
+    // $comm = $GLOBALS["communities"]->get_by_id($_SESSION["current_community"]);
+    // $posts = $GLOBALS["posts"]->get_by_community($comm, true, 10, (int) $match["offset"]);
+//
+    // $result = array();
+    // foreach($posts as $post) {
+        // $result[$post->id()] =
+    // }
+//
+    // echo json_encode();
+// }
