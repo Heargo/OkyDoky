2907666fe81d420ca19af99c88624e115b020ff6
Kroklethon <61271767+Kroklethon@users.noreply.github.com>
2021-04-01 14:08:13 +0200

    Update ajax.php

--- action/ajax.php
+++ action/ajax.php
@@ -4,7 +4,7 @@ function set_community(?array $match) {
     $_SESSION["current_community"] = (int) $_POST['id'];
     
 }
-function join_community(?array $match) {}
+function join_community(?array $match) {
     $id = (int) $_POST['id'];
     $tempcomm = new Community($DB, $id);
     $tempcomm->recruit(User::current());
