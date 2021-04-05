06f04a4746ce8b09cede5a2d958846d022a20c4d
romain22222 <romrom22222@hotmail.fr>
2021-04-05 16:49:09 +0200

    get_by_most_votes(Community, visible, limit, offset) qui renvoie les posts rangés par votes (différence de positifs négatifs)

--- class/PostManager.php
+++ class/PostManager.php
@@ -95,6 +95,54 @@ class PostManager {
 	}
 
 	/**
+	 * Get an array of posts by most votes
+	 *
+	 * @param bool|null $visible Filter if posts are marked as visible or not. Null will return both.
+	 * @param Community $c The community we are in
+	 * @param int $limit Limits the number of posts you want.
+	 * @param int $offset So you can get documents by slice of $limit. $offset should be a multiple of $limit.
+	 * @return Posts[] An array of posts.
+	 */
+	public function get_by_most_votes(Community $c, bool $visible = true, int $limit = 1, int $offset = 0) : array {
+		$visible = $visible ? 1 : 0;
+		$sql = 'SELECT p.id_%1$s, vPlus.c, vMinus.c
+				FROM `%1$s` p 
+				LEFT OUTER JOIN 
+					(SELECT id_%1$s, COUNT(v1.mark) AS c
+						FROM `%1$s` p1
+						JOIN `%2$s` v1 ON v1.post = p1.id_%1$s 
+						WHERE p1.visible = %3$d 
+						AND p1.community = %4$d
+						AND v1.mark = "%5$s" 
+						GROUP BY v1.post) 
+					vPlus
+				ON vPlus.id_%1$s = p.id_%1$s
+				LEFT OUTER JOIN 
+					(SELECT id_%1$s, COUNT(v2.mark) AS c
+						FROM `%1$s` p2 
+						JOIN `%2$s` v2 ON v2.post = p2.id_%1$s
+						WHERE p2.visible = %3$d
+						AND p2.community = %4$d 
+						AND v2.mark = "%6$s" 
+						GROUP BY v2.post)
+					vMinus
+				ON vMinus.id_%1$s = p.id_%1$s
+				WHERE p.visible = %3$d 
+				AND p.community = %4$d
+				ORDER BY (IFNULL(vPlus.c, 0) - IFNULL(vMinus.c, 0)) DESC 
+				LIMIT %7$d OFFSET %8$d';
+		$sql = sprintf($sql, Config::TABLE_POST, Config::TABLE_VOTE, $visible, $c->id(),"up", "down", $limit, $offset);
+		$result = $this->_db->query($sql);
+		if($result) {
+			for ($list = array();
+				 $row = $result->fetch_assoc();
+				 $list[] = new Post($this->_db, $row['id_' . Config::TABLE_POST]));
+			return $list;
+		}
+		return array();
+	}
+
+	/**
 	  * Delete a post based on its ID
 	  *
 	  * @param Post $post The new deleted post.
