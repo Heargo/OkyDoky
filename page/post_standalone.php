7646584f82d6cc1bc77f397f0e975fb5cd57f19c
Kroklethon <61271767+Kroklethon@users.noreply.github.com>
2021-04-12 14:26:34 +0200

    Added percentage

--- page/post_standalone.php
+++ page/post_standalone.php
@@ -49,7 +49,7 @@
 					}else{
 						?><p id="prctQualityText-<?=$post->id()?>" class="prctQuality red"><?php 
 					}
-					echo $prct ?>
+					echo $prct."%"?>
 					</p>
 
 				<!-- <a href="#"><img src="./img/svg/like.svg"></a>
