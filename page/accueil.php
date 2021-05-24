2801368a37bb94fe7188802960bec41362b32732
Simon LEONARD <git-1001af4@sinux.sh>
2021-05-24 18:42:40 +0200

    Spelling: checked!

--- page/accueil.php
+++ page/accueil.php
@@ -15,7 +15,7 @@
 		<a href="<?=Routes::url_for('/');?>" class="top-left-name">OkyDoky</a>
 		<a href="./login" class="l-sButton">Sign-up/Login</a>
 	</nav>
-	<svg class="bg-element-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#4bc0c8" d="M0,128L80,122.7C160,117,320,107,480,133.3C640,160,800,224,960,229.3C1120,235,1280,181,1360,154.7L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>	
+	<svg class="bg-element-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#4bc0c8" d="M0,128L80,122.7C160,117,320,107,480,133.3C640,160,800,224,960,229.3C1120,235,1280,181,1360,154.7L1440,128L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
 	</svg>
 
 	<div class="container">
@@ -33,19 +33,19 @@
 			<div data-aos="fade-right" data-aos-delay="200" class="img" style="background-image:url('./img/community.png')"></div>
 			<div data-aos="fade-up" data-aos-delay="200" class="img-desc">
 				<h3>Créez votre communauté !</h3>
-				<p>Etudiant, Fans de cuisine ou de science ? Créez la communauté qui vous resemble.</p>
+				<p>Etudiant, fans de cuisine ou de science ? Créez la communauté qui vous ressemble.</p>
 				<svg class="bubble" xmlns="http://www.w3.org/2000/svg">
 				<path fill="#42db75" d="M58.1,2.2C58.1,18.3,29,36.5,7.2,36.5C-14.5,36.5,-29.1,18.3,-29.1,2.2C-29.1,
 				-13.8,-14.5,-27.5,7.2,-27.5C29,-27.5,58.1,-13.8,58.1,2.2Z" transform="translate(100,155) scale(3.2)"/>
 				</svg>
-			</div>	
-			
+			</div>
+
 		</div>
 		<div class="reverse point-container ">
 			<div data-aos="fade-left" data-aos-delay="400" class="img" style="background-image:url('./img/share.png')"></div>
 			<div data-aos="fade-up" data-aos-delay="400" class="img-desc">
 				<h3>Partagez vos documents !</h3>
-				<p>Utilisez votre communauté pour partagez vos documents importants.</p>
+				<p>Utilisez votre communauté pour partager vos documents importants.</p>
 				<svg class="bubble" xmlns="http://www.w3.org/2000/svg">
 					<path fill="#4BC0C8" d="M32.6,-22.6C46.8,-8.2,66,5.6,64.4,15.7C62.9,25.7,40.6,31.9,19.8,42.4C-1,52.9,-20.3,67.6,-36.3,64.2C-52.2,60.8,-64.7,39.2,-67.8,17.8C-70.8,-3.7,-64.4,-24.9,-51.5,-39.1C-38.6,-53.2,-19.3,-60.1,-5.1,-56.1C9.2,-52.1,18.4,-37,32.6,-22.6Z" transform="translate(160 170) scale(2)" />
 				</svg>
@@ -62,7 +62,7 @@
 				</svg>
 			</div>
 		</div>
-		
+
 	</section>
 	<div data-aos="zoom-in" data-aos-delay="400" class="container-join">
 		<div class="box">
@@ -81,6 +81,6 @@
 <script>
   AOS.init();
 </script>
-</html>	
+</html>
 <!-- Uicons by <a href="https://www.flaticon.com/uicons" title="Flaticon">Flaticon</a> -->
 <!-- Illustration by Freepik Storyset -->
--- page/cgu.php
+++ page/cgu.php
@@ -19,8 +19,8 @@
 
 	<h1>Politique de confidentialité et cookies</h1>
     <div>
-    	<h2>À propos de Okydoky</h2>
-        <p>Okydoky est un projet pédagogique créer dans le cadre d'un cours d'informatique à l'<a href="https://www.univ-smb.fr/">Université Savoie Mont-Blanc</a></p>
+    	<h2>À propos de OkyDoky</h2>
+        <p>OkyDoky est un projet pédagogique créé dans le cadre d'un cours d'informatique à l'<a href="https://www.univ-smb.fr/">Université Savoie Mont-Blanc</a>.</p>
         <p>
         Ce projet est porté par les personnes suivantes :
             <ul class="team">
@@ -31,35 +31,35 @@
                 <li><img src="<?=Routes::url_for('/img/devs/juliette.jpg');?>" alt="Juliette"><p>Juliette NEYRAT</p></li>
             </ul>
         </p>
-        <p>Le site web est hébergé et géré par Simon LEONARD, que vous pouvez contacter à l'adresse <a href='mailto:okydoky@sinux.sh'>okydoky@sinux.sh</a> </p>
+        <p>Le site web est hébergé et géré par Simon LEONARD, que vous pouvez contacter à l'adresse <a href='mailto:okydoky@sinux.sh'>okydoky@sinux.sh</a>.</p>
 
         <h2>Collecte des données</h2>
-        <p>Okydoky collecte des données personnelles :
+        <p>OkyDoky collecte des données personnelles :
             <ul class="ls">
                 <li>lorsque vous créez un compte</li>
-                <li>lorsque vous postez des documents, envoyez des messages à une communauté ou toute autre forme de participation sur le site</li>
+                <li>lorsque vous postez des documents, envoyez des messages à une communauté ou effectuez toute autre forme de participation sur le site.</li>
             </ul>
         </p>
 
         <h2>Où sont stockées vos données ?</h2>
         <p>
-            Toute les données d'OkyDoky sont stocké chez OVH, en France.
+            Toutes les données d'OkyDoky sont stockées chez OVH, en France.
         </p>
 
         <h2>Comment puis-je suppimer mes données ?</h2>
         <p>
-            Étant un projet universitaire sur quelques mois, il se peut que nous n'ayons pas pensé à toute les réglementations.
-            Si vous souhaitez voir, télécharger ou supprimer vos données, envoyez un mail à <a href='mailto:okydoky@sinux.sh'>okydoky@sinux.sh</a>
+            Étant un projet universitaire sur quelques mois, il se peut que nous n'ayons pas pensé à toutes les réglementations.
+            Si vous souhaitez voir, télécharger ou supprimer vos données, envoyez un mail à <a href='mailto:okydoky@sinux.sh'>okydoky@sinux.sh</a>.
         </p>
 
         <h2>Gestion des cookies</h2>
-        <p>Tout les cookies utilisé par Okydoky sont purement de l'ordre technique.
+        <p>Touts les cookies utilisés par OkyDoky sont purement de l'ordre technique.
         Ils identifient de manière unique votre navigateur lorsque vous parcourez le site,
-        et permet d'identifier l'auteur (tel que vous vous êtes enregistrez) lorsque vous intéragissez avec le site</p>
+        et permettent d'identifier l'auteur (tel que vous vous êtes enregistré) lorsque vous intéragissez avec le site.</p>
         <p>Par conséquant, le seul cookie utilisé est PHPSESSID, un cookie propre à PHP.</p>
         <p>
-        Okydoky utilise aussi le <i>local storage</i> afin d'accéléré le chargement du site à travers un méchanisme de cache.
-        Voici la liste des clés :
+        OkyDoky utilise aussi le <i>local storage</i> afin d'accélérer le chargement du site à travers un méchanisme de cache.
+        Voici la liste des clés utilisées :
         <ul class="ls">
             <li>community</li>
             <li>offset</li>
